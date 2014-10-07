50107445c1a20508379f2b40f76a9efab011684c
Rich Middleditch <richmiddleditch@gmail.com>
2014-10-07 19:18:50 +0100

    Switch to deb file

--- mycookbook/attributes/nodejs.rb
+++ mycookbook/attributes/nodejs.rb
@@ -1,3 +1,3 @@
-default['nodejs']['version'] = 'nodejs-dbg_0.11.13-1chl1~trusty1_amd64.deb'
+default['nodejs']['version'] = 'nodejs_0.11.13-1chl1~trusty1_amd64.deb'
 default['nodejs']['dir'] = '/usr/local'
 default['nodejs']['src_url'] = "http://ppa.launchpad.net/chris-lea/node.js-devel/ubuntu/pool/main/n/nodejs"
--- mycookbook/recipes/nodejs.rb
+++ mycookbook/recipes/nodejs.rb
@@ -8,15 +8,13 @@ end
 # Let the user override the source url in the attributes
 nodejs_src_url = "#{node['nodejs']['src_url']}/#{node['nodejs']['version']}"
 
-remote_file "/usr/local/src/#{node['nodejs']['version']}" do
+remote_file "/tmp/#{node['nodejs']['version']}" do
   source nodejs_src_url
-  mode 0777
+  mode 0644
   action :create_if_missing
 end
 
-cookbook_file "/usr/local/src/#{node['nodejs']['version']}" do
-    source "#{node['nodejs']['version']}"
-    owner "root"
-    group "root"
-    mode "0444"
+dpkg_package "nodejs" do
+    source "/tmp/#{node['nodejs']['version']}"
+    action :install
 end
