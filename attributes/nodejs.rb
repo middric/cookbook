806f4fbfcd205bb5614e857d0231b81c1035aa04
Rich Middleditch <richard.middleditch1@bbc.co.uk>
2014-10-07 17:49:05 +0100

    Create nodejs.rb

---
+++ attributes/nodejs.rb
@@ -0,0 +1,4 @@
+default['nodejs']['version'] = '0.11.13'
+default['nodejs']['checksum'] = 'da4a9adb73978710566f643241b2c05fb8a97574'
+default['nodejs']['dir'] = '/usr/local'
+default['nodejs']['src_url'] = "http://nodejs.org/dist"
