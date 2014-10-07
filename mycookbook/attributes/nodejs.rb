a49fe6d72c5d206ba656b9a57dba15ed55fbd861
Rich Middleditch <richmiddleditch@gmail.com>
2014-10-07 19:01:21 +0100

    Switch to deb file

--- mycookbook/attributes/nodejs.rb
+++ mycookbook/attributes/nodejs.rb
@@ -1,4 +1,3 @@
-default['nodejs']['version'] = '0.11.13'
-default['nodejs']['checksum'] = 'da4a9adb73978710566f643241b2c05fb8a97574'
+default['nodejs']['version'] = 'nodejs-dbg_0.11.13-1chl1~trusty1_amd64.deb'
 default['nodejs']['dir'] = '/usr/local'
-default['nodejs']['src_url'] = "http://nodejs.org/dist"
+default['nodejs']['src_url'] = "http://ppa.launchpad.net/chris-lea/node.js-devel/ubuntu/pool/main/n/nodejs"
--- mycookbook/recipes/nodejs.rb
+++ mycookbook/recipes/nodejs.rb
@@ -5,41 +5,18 @@ case node['platform_family']
     package "libssl-dev"
 end
 
-nodejs_tar = "node-v#{node['nodejs']['version']}.tar.gz"
-nodejs_tar_path = nodejs_tar
-if node['nodejs']['version'].split('.')[1].to_i >= 5
-  nodejs_tar_path = "v#{node['nodejs']['version']}/#{nodejs_tar_path}"
-end
 # Let the user override the source url in the attributes
-nodejs_src_url = "#{node['nodejs']['src_url']}/#{nodejs_tar_path}"
+nodejs_src_url = "#{node['nodejs']['src_url']}/#{node['nodejs']['version']}"
 
-remote_file "/usr/local/src/#{nodejs_tar}" do
+remote_file "/usr/local/src/#{node['nodejs']['version']}" do
   source nodejs_src_url
-  checksum node['nodejs']['checksum']
-  mode 0644
+  mode 0777
   action :create_if_missing
 end
 
-# --no-same-owner required overcome "Cannot change ownership" bug
-# on NFS-mounted filesystem
-execute "tar --no-same-owner -zxf #{nodejs_tar}" do
-  cwd "/usr/local/src"
-  creates "/usr/local/src/node-v#{node['nodejs']['version']}"
-end
-
-bash "compile node.js" do
-  cwd "/usr/local/src/node-v#{node['nodejs']['version']}"
-  code <<-EOH
-    PATH="/usr/local/bin:$PATH"
-    ./configure --prefix=#{node['nodejs']['dir']} && \
-    make
-  EOH
-  creates "/usr/local/src/node-v#{node['nodejs']['version']}/node"
-end
-
-execute "nodejs make install" do
-  environment({"PATH" => "/usr/local/bin:/usr/bin:/bin:$PATH"})
-  command "make install"
-  cwd "/usr/local/src/node-v#{node['nodejs']['version']}"
-  not_if {File.exists?("#{node['nodejs']['dir']}/bin/node") && `#{node['nodejs']['dir']}/bin/node --version`.chomp == "v#{node['nodejs']['version']}" }
+cookbook_file "/usr/local/src/#{node['nodejs']['version']}" do
+    source "#{node['nodejs']['version']}"
+    owner "root"
+    group "root"
+    mode "0444"
 end
