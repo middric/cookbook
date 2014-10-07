05c393ae731f0e9f095ed69943ce3c3f3b8fcc09
Rich Middleditch <richmiddleditch@gmail.com>
2014-10-07 18:17:37 +0100

    Add metadata.rb

---
+++ mycookbook/metadata.rb
@@ -0,0 +1,12 @@
+name              "nodejs"
+maintainer        "Rich Middleditch"
+maintainer_email  "richmiddleditch@gmail.com"
+license           "MIT"
+description       "Installs and configures node.js"
+version           "0.0.1"
+
+recipe "nodejs", "Installs nginx package and sets up configuration with Debian apache style with sites-enabled/sites-available"
+
+%w{ ubuntu debian centos redhat amazon scientific oracle fedora }.each do |os|
+ supports os
+end
