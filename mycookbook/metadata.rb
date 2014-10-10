44f8a5ea597ad38a7aec9c49ec11fecaba964e33
Rich Middleditch <richmiddleditch@gmail.com>
2014-10-10 09:09:39 +0100

    a

--- mycookbook/metadata.rb
+++ mycookbook/metadata.rb
@@ -4,6 +4,7 @@ maintainer_email  "richmiddleditch@gmail.com"
 license           "MIT"
 description       "Installs and configures node.js"
 version           "0.0.1"
+depends          'build-essential', '~> 1.4.0'
 
 recipe "nodejs", "Installs nginx package and sets up configuration with Debian apache style with sites-enabled/sites-available"
 
